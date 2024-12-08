51b0848f309c7869ba2f1597247625344df06dcc
rot4tion <nhonhoem0123@gmail.com>
2024-12-08 10:57:37 +0700

    clean index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import { logDebug, readFile } from "./util";
+import { logDebug } from "./util";
 
 function main() {
   console.log("Script Running");
-  console.log(
-    readFile(
-      "/var/mobile/Documents/Screenshots/confirm-tool.PNG"
-    )
-  );
-
   alert(
     "Can you give me a github star for this <3"
   );
